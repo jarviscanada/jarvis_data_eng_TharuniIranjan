1fec35dc866048ff9054cd55acfed11eacffbf9b
Tharuni Iranjan <tharuni.iv@hotmail.ca>
2024-04-18 19:38:50 -0400

    MarketDataDao config

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -0,0 +1,17 @@
+package ca.jrvs.apps.trading.service;
+
+import ca.jrvs.apps.trading.entity.IexQuote;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuoteService {
+    /**
+     * Find an IexQuote
+     * @param ticker
+     * @return IexQuote object
+     * @throws IllegalArgumentException if ticker is invalid
+     */
+//    public IexQuote findIexQuoteByTicker(String ticker) {
+//        //TODO
+//    }
+}
---
+++ springboot/src/test/java/ca/jrvs/apps/jdbc/MarketDataDaoIntTest.java
@@ -0,0 +1,13 @@
+package ca.jrvs.apps.jdbc;
+
+import ca.jrvs.apps.trading.dao.MarketDataDao;
+import org.junit.Before;
+
+public class MarketDataDaoIntTest {
+    private MarketDataDao dao;
+
+    @Before
+    public void init(){
+
+    }
+}
