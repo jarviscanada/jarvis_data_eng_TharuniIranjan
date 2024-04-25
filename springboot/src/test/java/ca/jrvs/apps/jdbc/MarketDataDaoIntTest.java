3d2acc33206e89c4dd278bce972b9227c17a0c73
Tharunii <tharuni.iv@hotmail.ca>
2024-04-25 17:10:58 -0400

    day 3 updates

--- springboot/src/test/java/ca/jrvs/apps/jdbc/MarketDataDaoIntTest.java
+++ springboot/src/test/java/ca/jrvs/apps/jdbc/MarketDataDaoIntTest.java
@@ -1,13 +1,55 @@
 package ca.jrvs.apps.jdbc;
 
+import ca.jrvs.apps.trading.config.MarketDataConfig;
 import ca.jrvs.apps.trading.dao.MarketDataDao;
+import ca.jrvs.apps.trading.entity.IexQuote;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class MarketDataDaoIntTest {
     private MarketDataDao dao;
 
     @Before
     public void init(){
+        CloseableHttpClient httpClient = HttpClients.createDefault();;
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
 
+        dao = new MarketDataDao(httpClient, marketDataConfig);
+    }
+
+    @Test
+    public void findByTicker() {
+        String ticker = "AAPL";
+        IexQuote iexQuote = dao.findById(ticker).get();
+        assertEquals(ticker, iexQuote.getSymbol());
+    }
+
+    @Test
+    public void findIexQuotesByTickers() throws IOException {
+        List<IexQuote> quoteList = dao.findAllById(Arrays.asList("AAPL", "FB"));
+        assertEquals(2, quoteList.size());
+        assertEquals("AAPL", quoteList.get(0).getSymbol());
+
+        try {
+            dao.findAllById(Arrays.asList("AAPL", "FB2"));
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
