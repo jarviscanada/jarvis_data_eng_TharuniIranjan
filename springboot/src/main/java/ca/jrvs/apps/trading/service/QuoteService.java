59cc6859c2980eee0ae5c46852516875c455645a
Tharunii <tharuni.iv@hotmail.ca>
2024-05-06 17:15:41 -0400

    QuoteService update

--- springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -1,23 +1,30 @@
 package ca.jrvs.apps.trading.service;
 
 import ca.jrvs.apps.trading.dao.MarketDataDao;
+import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.entity.IexQuote;
+import ca.jrvs.apps.trading.model.Quote;
 import ca.jrvs.apps.trading.util.TradingAppTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class QuoteService {
     private final MarketDataDao marketDataDao;
+    private final QuoteDao quoteDao;
     private static Logger logger = LoggerFactory.getLogger(QuoteService.class);
 
     @Autowired
-    public QuoteService(MarketDataDao marketDataDao) {
+    public QuoteService(MarketDataDao marketDataDao, QuoteDao quoteDao) {
         this.marketDataDao = marketDataDao;
+        this.quoteDao = quoteDao;
     }
 
     /**
@@ -40,6 +47,74 @@ public class QuoteService {
         }
         logger.info("Successful call on QuoteService.findIexQuoteByTicker for ticker '" + ticker + "'");
         return iexQuoteOptional.get();
+    }
+
+    public void updateMarketData() {
+        List<Quote> quotes = quoteDao.findAll();
+        List<Quote> updatedQuotes = new ArrayList<>();
+        for (Quote quote : quotes) {
+            Optional<IexQuote> iexQuoteOptional = marketDataDao.findById(quote.getId());
+            if (iexQuoteOptional.isPresent()) {
+                IexQuote iexQuote = iexQuoteOptional.get();
+                Quote updatedQuote = buildQuote(iexQuote);
+                updatedQuotes.add(updatedQuote);
+            }
+        }
+        quoteDao.saveAll(updatedQuotes);
+    }
+
+    public List<Quote> saveQuotes(List<String> tickers) {
+        List<Quote> savedQuotes = new ArrayList<>();
+        for (String ticker : tickers) {
+            IexQuote iexQuote = marketDataDao.findById(ticker).orElseThrow(IllegalArgumentException::new);
+            Quote quote = buildQuote(iexQuote);
+            savedQuotes.add(quoteDao.save(quote));
+        }
+        return savedQuotes;
+    }
+
+    public Quote saveQuote(Quote quote) {
+        return quoteDao.save(quote);
+    }
+
+    public List<Quote> findAllQuotes() {
+        return quoteDao.findAll();
+    }
+
+    private Quote buildQuote(IexQuote iexQuote) {
+        Quote quote = new Quote();
+        quote.setId(iexQuote.getSymbol());
+        quote.setLastPrice(iexQuote.getLatestPrice());
+        quote.setBidPrice(iexQuote.getIexBidPrice());
+        quote.setAskPrice(iexQuote.getIexAskPrice());
+        quote.setBidSize(iexQuote.getIexBidSize());
+        quote.setAskSize(iexQuote.getIexAskSize());
+        return quote;
+    }
+
+    protected static Quote buildQuoteFromIexQuote(IexQuote iexQuote) {
+        if (iexQuote == null) {
+            throw new IllegalArgumentException("IexQuote cannot be null");
+        }
+
+        Quote quote = new Quote();
+        quote.setId(iexQuote.getSymbol());
+        quote.setLastPrice(Optional.ofNullable(iexQuote.getLatestPrice()).orElse(0.0));
+        quote.setBidPrice(Optional.ofNullable(iexQuote.getIexBidPrice()).orElse(0.0));
+        quote.setAskPrice(Optional.ofNullable(iexQuote.getIexAskPrice()).orElse(0.0));
+        quote.setBidSize(Optional.ofNullable(iexQuote.getIexBidSize()).orElse(0));
+        quote.setAskSize(Optional.ofNullable(iexQuote.getIexAskSize()).orElse(0));
+        return quote;
+    }
+
+    protected Quote saveQuote(String ticker) {
+        if (ticker == null || ticker.isEmpty()) {
+            throw new IllegalArgumentException("Ticker cannot be null or empty");
+        }
+
+        IexQuote iexQuote = marketDataDao.findById(ticker).orElseThrow(IllegalArgumentException::new);
+        Quote quote = buildQuoteFromIexQuote(iexQuote);
+        return quoteDao.save(quote);
     }
 
 }
